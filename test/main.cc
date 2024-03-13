bf3320322723c29a522bab050b4786f7e8e8646c
Haijun Yu <yuhj@zetyun.com>
2024-03-13 14:31:12 +0800

    update code

--- test/main.cc
+++ test/main.cc
@@ -13,38 +13,71 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <cstddef>
 #include <iostream>
 
-#include "fbs_test.h"
-#include "pb_test.h"
+#include "fbs_test.h"  // NOLINT
+#include "pb_test.h"   // NOLINT
 
 int main(int argc, char *argv[]) {
-  std::cout << "main program" << std::endl;
-
   std::string buffer;
-  int times = 10;
+  int times = 100;
   int64_t pb_serialization_time_ms = 0;
   int64_t pb_deserialization_time_ms = 0;
   int64_t fbs_serialization_time_ms = 0;
   int64_t fbs_deserialization_time_ms = 0;
+  std::string type = "all";
 
-  pb_serialization(times, buffer, pb_serialization_time_ms);
-  pb_deserialization(buffer, pb_deserialization_time_ms);
+  std::string tmp_type;
 
-  std::cout << "pb_serialization : " << pb_serialization_time_ms << " ms"
+  if (argc > 1) {
+    tmp_type = argv[1];
+  }
+
+  if (tmp_type == std::string("pb")) {
+    type = "pb";
+  } else if (tmp_type == std::string("fbs")) {
+    type = "fbs";
+  } else if (tmp_type == std::string("all")) {
+    type = "all";
+  }
+
+  if (argc > 2) {
+    int tmp_times = 0;
+    try {
+      int tmp_times = atoi(argv[2]);
+      times = tmp_times;
+    } catch (...) {
+    }
+  }
+
+  std::cout << "using : argv[1] is type [pb|fbs|all] default all. argv[2] is "
+               "times default 100."
             << std::endl;
 
-  std::cout << "pb_deserialization : " << pb_deserialization_time_ms << " ms"
-            << std::endl;
+  std::cout << "type : " << type << " times : " << times << std::endl;
 
-  fbs_serialization(times, buffer, fbs_serialization_time_ms);
-  fbs_deserialization(buffer, fbs_deserialization_time_ms);
+  if (std::string("all") == type || std::string("pb") == type) {
+    pb_serialization(times, buffer, pb_serialization_time_ms);
+    pb_deserialization(buffer, pb_deserialization_time_ms);
 
-  std::cout << "fbs_serialization : " << fbs_serialization_time_ms << " ms"
-            << std::endl;
+    std::cout << "protobuf   serialization   : " << pb_serialization_time_ms
+              << " ms" << std::endl;
 
-  std::cout << "fbs_deserialization : " << fbs_deserialization_time_ms << " ms"
-            << std::endl;
+    std::cout << "protobuf   deserialization : " << pb_deserialization_time_ms
+              << " ms" << std::endl;
+  }
+
+  if (std::string("all") == type || std::string("fbs") == type) {
+    fbs_serialization(times, buffer, fbs_serialization_time_ms);
+    fbs_deserialization(buffer, fbs_deserialization_time_ms);
+
+    std::cout << "flatbuffer serialization   : " << fbs_serialization_time_ms
+              << " ms" << std::endl;
+
+    std::cout << "flatbuffer deserialization : " << fbs_deserialization_time_ms
+              << " ms" << std::endl;
+  }
 
   return 0;
 }
